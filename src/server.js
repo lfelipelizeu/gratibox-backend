0497110a59dba3fc393c1545917bc7d1534c9aef
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-17 14:45:26 -0300

    Fix the type module and server port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
-import app from './app';
+import app from './app.js';
 
-app.listen(process.env.PORT, () => {
+const port = 4000;
+
+app.listen(port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${port}`);
 });
