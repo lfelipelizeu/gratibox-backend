1198108d09639442979dbb526b5b54a4da1c24aa
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-22 12:27:48 -0300

    Create a subscription function and post request to /subscrube

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { signUp, logIn } from './controllers/user.js';
 import { getPlan } from './controllers/plans.js';
 import { getStates } from './controllers/adresses.js';
+import { newSubscription } from './controllers/subscriptions.js';
 
 const app = express();
 app.use(cors());
@@ -15,5 +16,7 @@ app.post('/login', logIn);
 app.get('/plan', getPlan);
 
 app.get('/states', getStates);
+
+app.post('/subscribe', newSubscription);
 
 export default app;
