cdef130e869ed68127f1f9222a80fc5ba4b57484
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-22 12:28:09 -0300

    Create a test to the subscription function

--- tests/app.test.js
+++ tests/app.test.js
@@ -183,3 +183,151 @@ describe('GET /plan', () => {
         expect(status).toEqual(200);
     });
 });
+
+describe('POST /subscribe', () => {
+    let session = {};
+
+    beforeAll(async () => {
+        session = await createSession();
+    });
+
+    afterAll(async () => {
+        await connection.query('DELETE FROM delivery_items;');
+        await connection.query('DELETE FROM deliveries;');
+        await connection.query('DELETE FROM subscriptions;');
+        await connection.query('DELETE FROM adresses;');
+        await connection.query('DELETE FROM cities;');
+        await connection.query('DELETE FROM sessions;');
+        await connection.query('DELETE FROM users;');
+    });
+
+    it('return 401 for missing token', async () => {
+        const body = {
+            plan: 'Semanal',
+            deliveryDay: faker.datatype.number({ min: 1, max: 6 }),
+            items: [faker.datatype.number({ min: 1, max: 3 })],
+            shippingAdress: {
+                name: faker.name.findName(),
+                adress: faker.address.streetAddress(),
+                zipcode: faker.address.zipCode().split('-').join(''),
+                city: faker.address.cityName(),
+                state: faker.datatype.number({ min: 28, max: 54 }),
+            },
+        };
+
+        const result = await agent
+            .post('/subscribe')
+            .send(body);
+        const { status } = result;
+
+        expect(status).toEqual(401);
+    });
+
+    it('return 400 for invalid body', async () => {
+        const header = {
+            token: session.token,
+        };
+
+        const body = {
+            plan: 'Semanal',
+            deliveryDay: faker.datatype.number({ min: 1, max: 6 }),
+            items: [],
+            shippingAdress: {
+                name: faker.name.findName(),
+                adress: faker.address.streetAddress(),
+                zipcode: faker.address.zipCode().split('-').join(''),
+                city: faker.address.cityName(),
+                state: faker.datatype.number({ min: 28, max: 54 }),
+            },
+        };
+
+        const result = await agent
+            .post('/subscribe')
+            .send(body)
+            .set('authorization', header.token);
+        const { status } = result;
+
+        expect(status).toEqual(400);
+    });
+
+    it('return 401 for invalid token', async () => {
+        const header = {
+            token: faker.datatype.uuid(),
+        };
+
+        const body = {
+            plan: 'Semanal',
+            deliveryDay: faker.datatype.number({ min: 1, max: 6 }),
+            items: [faker.datatype.number({ min: 1, max: 3 })],
+            shippingAdress: {
+                name: faker.name.findName(),
+                adress: faker.address.streetAddress(),
+                zipcode: faker.address.zipCode().split('-').join(''),
+                city: faker.address.cityName(),
+                state: faker.datatype.number({ min: 28, max: 54 }),
+            },
+        };
+
+        const result = await agent
+            .post('/subscribe')
+            .send(body)
+            .set('authorization', header.token);
+        const { status } = result;
+
+        expect(status).toEqual(401);
+    });
+
+    it('return 404 for invalid plan', async () => {
+        const header = {
+            token: session.token,
+        };
+
+        const body = {
+            plan: 'Diário',
+            deliveryDay: faker.datatype.number({ min: 1, max: 6 }),
+            items: [faker.datatype.number({ min: 1, max: 3 })],
+            shippingAdress: {
+                name: faker.name.findName(),
+                adress: faker.address.streetAddress(),
+                zipcode: faker.address.zipCode().split('-').join(''),
+                city: faker.address.cityName(),
+                state: faker.datatype.number({ min: 28, max: 54 }),
+            },
+        };
+
+        const result = await agent
+            .post('/subscribe')
+            .send(body)
+            .set('authorization', header.token);
+        const { status } = result;
+
+        expect(status).toEqual(404);
+    });
+
+    it('return 201', async () => {
+        const header = {
+            token: session.token,
+        };
+
+        const body = {
+            plan: 'Semanal',
+            deliveryDay: faker.datatype.number({ min: 1, max: 6 }),
+            items: [faker.datatype.number({ min: 1, max: 3 })],
+            shippingAdress: {
+                name: faker.name.findName(),
+                adress: faker.address.streetAddress(),
+                zipcode: faker.address.zipCode().split('-').join(''),
+                city: faker.address.cityName(),
+                state: faker.datatype.number({ min: 28, max: 54 }),
+            },
+        };
+
+        const result = await agent
+            .post('/subscribe')
+            .send(body)
+            .set('authorization', header.token);
+        const { status } = result;
+
+        expect(status).toEqual(201);
+    });
+});
